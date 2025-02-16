69b0f8ec106d69a6b92833d8d64fa1334027d4ae
tutateuta <tutateuta@gmail.com>
2025-02-17 11:59:10 +1300

    uploading files

---
+++ js/main.js
@@ -0,0 +1,33 @@
+var image = document.getElementsByClassName("img-parallax-1");
+new simpleParallax(image, {
+  scale: 1.5,
+});
+
+var image = document.getElementsByClassName("img-parallax-2");
+new simpleParallax(image, {
+  scale: 1.5,
+  // orientation: 'left'
+});
+
+var image = document.getElementsByClassName("img-parallax-3");
+new simpleParallax(image, {
+  scale: 1.5,
+  // orientation: 'right'
+});
+
+var image = document.getElementsByClassName("img-parallax-4");
+new simpleParallax(image, {
+  scale: 1.5,
+});
+
+var image = document.getElementsByClassName("img-parallax-5");
+new simpleParallax(image, {
+  scale: 1.5,
+  //  orientation: 'right'
+});
+
+var image = document.getElementsByClassName("img-parallax-6");
+new simpleParallax(image, {
+  scale: 1.5,
+  // orientation: 'left'
+});
